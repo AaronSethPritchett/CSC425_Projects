00573d8655d38df9563a8814b9ed7f62e2037568
charton11 <charton@murraystate.edu>
2017-11-06 08:58:36 -0600

    Add files via upload

--- public/play.js
+++ public/play.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-    var currentMarker;      
+    var currentMarker;
+    var yourTurn;
     var firstLoad = true;
     var box1status = false;
     var box2status = false;
@@ -26,69 +27,129 @@ window.onload = function() {
     socket.on('message', function (data) {
         if(firstLoad == true) {
             currentMarker = data.message;
+            if(currentMarker == "X")
+                yourTurn = true;
+            else
+                yourTurn = false;
             firstLoad = false;
         } else {
             console.log(data.message);
+            yourTurn == !yourTurn;
             var cell = data.message.charAt(0);
             var fill = data.message.charAt(1);
-            if(cell == 1)
+            if(cell == "1")
                 {
                     box1status = true;
-                    //include if statement for X or O to find what to replace the cell with for css
-                }
+                if(fill == "X")
+                    box1.className += "X";
+                else if(fill == "O")
+                    box1.className += "O";
+                else
+                    console.log("Faulty background change!");
+            }
+                
             else if(cell == "2")
                 {
                     box2status = true;
+                if(fill == "X")
+                    box2.className += "X";
+                else if(fill == "O")
+                    box2.className += "O";
+                else
+                    console.log("Faulty background change!");
                 }
             else if(cell == "3")
                 {
                     box3status = true;
+                if(fill == "X")
+                    box3.className += "X";
+                else if(fill == "O")
+                    box3.className += "O";
+                else
+                    console.log("Faulty background change!");
                 }
             else if(cell == "4")
                 {
                     box4status = true;
+                if(fill == "X")
+                    box4.className += "X";
+                else if(fill == "O")
+                    box4.className += "O";
+                else
+                    console.log("Faulty background change!");
                 }
             else if(cell == "5")
                 {
                     box5status = true;
+                if(fill == "X")
+                    box5.className += "X";
+                else if(fill == "O")
+                    box5.className += "O";
+                else
+                    console.log("Faulty background change!");
                 }
             else if(cell == "6")
                 {
                     box6status = true;
+                if(fill == "X")
+                    box6.className += "X";
+                else if(fill == "O")
+                    box6.className += "O";
+                else
+                    console.log("Faulty background change!");
                 }
             else if(cell == "7")
                 {
                     box7status = true;
+                if(fill == "X")
+                    box7.className += "X";
+                else if(fill == "O")
+                    box7.className += "O";
+                else
+                    console.log("Faulty background change!");
                 }
             else if(cell == "8")
                 {
                     box8status = true;
+                if(fill == "X")
+                    box8.className += "X";
+                else if(fill == "O")
+                    box8.className += "O";
+                else
+                    console.log("Faulty background change!");
                 }
             else if(cell == "9")
                 {
                     box9status = true;
+                if(fill == "X")
+                    box9.className += "X";
+                else if(fill == "O")
+                    box9.className += "O";
+                else
+                    console.log("Faulty background change!");
                 }
             else
             {
-                console.log("Faulty message recieved!");    
+                console.log("Faulty message recieved! " + data.message);    
             }
         }
     });
 
     box1.onclick = function() {
-        if(box1status==false)
-            {
+        if(box1status==false && yourTurn == true)   //put yourturn here
+    {
                 var text = "1" + currentMarker;
                 socket.emit('send', { message: text });
                 box1status = true;
-            }
+                yourTurn = false;
     };
      box2.onclick = function() {
-        if(box2status==false)
+        if(box2status==false && yourTurn == true)
             {
                 var text = "2" + currentMarker;
                 socket.emit('send', { message: text });
                 box2status = true;
+                yourTurn == false;
             }
     };
      box3.onclick = function() {
@@ -149,4 +210,4 @@ window.onload = function() {
     };
     
 
-}
+}}
